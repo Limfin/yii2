cbfcdc9fcb5a9d3d6cde9bc6dcf97b63c72e8e06
Limfin <victor010389@yandex.ru>
2021-11-30 20:20:03 +0400

    Валидация формы. Именование label для инпутов в модели. Смена подписи ошибки для инпута с английского языка на русский в файле config/web.php. Валидация длины сообщения. Валидация email. Кастомный валидатор myrule(он срабатывает только на сервере, на клиенте(в браузере ошибка не отобразится). Псевдовалидатор trim (обрезает пробелы в начале и конце сообщения).

--- views/post/test.php
+++ views/post/test.php
@@ -20,13 +20,20 @@ debug($names);
 
 <!-- Форма -->
 <?php $form = ActiveForm::begin(['options' => ['id' => 'test_form', 'class' => 'form-horizontal']]) ?>
-<!-- Инпут со кастомным классом для подсказки(hint) -->
+
+<!-- Инпут с кастомным классом для подсказки(hint) -->
 <?= $form->field($model, 'name')->label('Имя')->hint('Подсказка(hint) к полю', ['class'=>'form-hint']) ?>
 <!----------------------------->
-<!-- Интпут с кастомным шаблоном("{label}\n{input}\n{hint}") и кастомным классом для инпута -->
-<?= $form->field($model, 'email', ['template' => "{label}\n{input}\n{hint}"])->input('email',['class'=>'form-control text-center']) ?>
+
+<!-- Инпут с кастомным классом -->
+<?= $form->field($model, 'email')->input('email',['class'=>'form-control text-center']) ?>
 <!----------------------------->
-<?= $form->field($model, 'text')->label('Текст сообщения')->textarea(['rows' => 5]) ?>
+
+<?= $form->field($model, 'test')->label('Тестовое поле') ?>
+
+<!-- Инпут с кастомным шаблоном("{label}\n{input}\n{hint}") и кастомным классом для инпута -->
+<?= $form->field($model, 'text', ['template' => "{label}\n{input}\n{hint}"])->label('Текст сообщения')->textarea(['rows' => 5]) ?>
+<!----------------------------->
 <?= Html::submitButton('Отправить', ['class' => 'btn btn-success']) ?>
 <?php ActiveForm::end() ?>
 <!----------------------------->
